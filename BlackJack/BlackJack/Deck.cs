ca4c74ffa17acddcab38e326a19dca0fec84f274
scoophamp <hampus_cousin@hotmailcom>
2016-03-08 09:07:34 +0100

    Removed the enum

--- BlackJack/BlackJack/Deck.cs
+++ BlackJack/BlackJack/Deck.cs
@@ -10,30 +10,8 @@ namespace BlackJack
     {
         private List<Enum> MyCards = new List<Enum>(52);
         
-        private enum TheDeck
-        {
-            two = 2,
-            three = 3,
-            four = 4,
-            five = 5,
-            six = 6,
-            seven = 7,
-            eight = 8,
-            nine = 9,
-            ten = 10,
-            knight = 10,
-            queen = 10,
-            king = 10,
-            ace = 0,
-
-        }
-        private enum SortsofCards
-        {
-            Heart,
-            Spades,
-            Club,
-            Diamond
-        }
+    
+      
 
        
         public void TheCards() //Writes the cards to the list
